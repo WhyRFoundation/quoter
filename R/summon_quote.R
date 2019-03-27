91046cc5e074dbf873fa205d96ce23c54e2e74c8
krzyslom <krzysztofslomczynski@gmail.com>
2019-03-27 10:07:18 +0100

    Change data to csv and move it to inst folder

--- R/summon_quote.R
+++ R/summon_quote.R
@@ -10,5 +10,7 @@
 #' @examples
 #' summon_quote()
 summon_quote <- function() {
+  quotes <- readLines(system.file("extdata", "quotes.csv", package = "quoter"),
+                      encoding = "UTF-8")
   sample(quotes, 1)
 }
